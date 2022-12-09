c8657f69fb3d28793a023bb8b0224d7aa165a55d
Omarkhaled52 <96533395+Omarkhaled52@users.noreply.github.com>
2022-12-09 14:34:40 +0200

    yehia commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import SignIn from './pages/SignIn';
 import AddAdministrator from './pages/AddAdministrator';
 import AddInstructor from './pages/AddInstructor';
 import AddCorporateTrainees from './pages/AddCorporateTrainees';
+import CreateQuestion from './pages/CreateQuestion';
+import Exam from './pages/Exam';
+import InstructorExam from './pages/InstructorExam';
+import InstructorCourses from './pages/InstructorCourses';
+import InstructorCoursesTable from './pages/InstructorCoursesTable';
+import InstructorCoursesSearch from './pages/InstructorCoursesSearch';
+
+
 
 
 
@@ -16,9 +24,15 @@ function App() {
         <Routes>
 
           <Route path ="/" element ={<SignIn />}/>
-          <Route path ="/AddAdministrator" element ={<AddAdministrator />}/>
+          <Route path ="/s" element ={<AddAdministrator />}/>
           <Route path ="/AddInstructor" element ={<AddInstructor />}/>
           <Route path ="/AddCorporateTrainees" element ={<AddCorporateTrainees />}/>
+          <Route path ="/CreateQuestion" element ={<CreateQuestion />}/>
+          <Route path ="/Exam" element ={<Exam />}/>
+          <Route path ="/InstructorExam" element ={<InstructorExam />}/>
+          <Route path ="/InstructorCourses" element ={<InstructorCourses />}/>
+          <Route path ="/InstructorCoursesTable" element ={<InstructorCoursesTable />}/>
+          <Route path ="/InstructorCoursesSearch" element ={<InstructorCoursesSearch />}/>
 
 
 
